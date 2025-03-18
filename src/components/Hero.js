28794651237e85f32c86ebb6859d28df843dbfaa
David Prado <j.david.prado1716@gmail.com>
2025-03-18 18:32:25 -0400

    hero fix attempt 1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,21 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
 
-const collections = [
-  {
-    name: "Women's",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/home-page-04-collection-01.jpg",
-    imageAlt: "Woman wearing a comfortable cotton t-shirt.",
-  },
-  {
-    name: "Men's",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/home-page-04-collection-02.jpg",
-    imageAlt: "Man wearing a comfortable and casual cotton t-shirt.",
-  },
-  {
-    name: "Desk Accessories",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/home-page-04-collection-03.jpg",
-    imageAlt:
-      "Person sitting at a wooden desk with paper note organizer, pencil and tablet.",
-  },
-];
+import { motion, useScroll, useTransform } from "framer-motion";
+import hero from "../../public/images/hero.png";
+import { useEffect, useState } from "react";
 export function Hero() {
   const { scrollY } = useScroll();
   const opacity = useTransform(scrollY, [0, 500], [1, 0]);
   const backgroundSize = useTransform(scrollY, [0, 600], ["100%", "200%"]);
+
+  const [heroImage, setHeroImage] = useState(hero);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = hero.src;
+    image.onload = () => {
+      setHeroImage(hero);
+    };
+  }, []);
 
   return (
     // <div className="relative z-10 lg:h-screen md:h-[60vh] sm:h-[60vh]">
@@ -36,7 +24,7 @@ export function Hero() {
         style={{
           opacity,
           backgroundSize,
-          backgroundImage: "url('/images/hero.png')",
+          backgroundImage: `url(${heroImage.src})`,
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
         }}
@@ -52,23 +40,24 @@ export function Hero() {
         </div>
         <div className="relative text-center sm:pb-0 sm:h-[60vh] lg:h-screen">
           {/* Background image and overlap */}
-          <div
+          {/* <div
             aria-hidden="true"
             className="absolute inset-0 flex flex-col sm:hidden"
           >
             <div className="relative w-full flex-1 bg-gray-800">
               <div className="absolute inset-0 overflow-hidden">
-                <img
-                  alt=""
+                <Image
                   src="/images/hero.png"
-                  className="size-full object-cover filter blur-[2px]"
+                  layout="fill"
+                  style={{ objectFit: "cover", objectPosition: "center" }}
+                  alt=""
                 />
               </div>
               <div className="absolute inset-0 bg-black opacity-60" />
             </div>
-          </div>
+          </div> */}
           <div className="relative h-screen flex flex-col justify-center">
-            <div className='bg-black bg-opacity-50 py-6 px-20 rounded h-full w-full mx-auto justify-center items-center flex flex-col'>
+            <div className="bg-black bg-opacity-50 py-6 px-20 rounded h-full w-full mx-auto justify-center items-center flex flex-col">
               <h1 className="text-xl font-bold tracking-tight text-white sm:text-xl md:text-2xl pb-10">
                 What are you interested in?
               </h1>
@@ -119,7 +108,7 @@ export function Hero() {
                 </a>
               </div>
               {/* appointment and rent */}
-              <div className="mt-24 lg:hidden pt-15 flex flex-row justify-space-between">
+              {/* <div className="mt-24 lg:hidden pt-15 flex flex-row justify-space-between">
                 <div className="flex flex-1 items-center justify-center">
                   <button
                     type="button"
@@ -128,19 +117,11 @@ export function Hero() {
                     Appointment
                   </button>
                 </div>
-                <div className="flex flex-1 items-center justify-center">
-                  <button
-                    type="button"
-                    className="p-1 text-white-700 hover:text-gray-500 border-2 border-white "
-                  >
-                    Rent a studio
-                  </button>
-                </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
       </motion.div>
     </div>
-  )
+  );
 }
